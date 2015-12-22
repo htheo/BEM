dd2f1ecb12ffdc5052bb386906265bd14354a977
Theo Hinfray <hinfraytheo@gmail.com>
2015-12-22 13:36:18 +0100

    ajout des pages materiaux et partanaires sur la branche back

--- materiaux/cendre_volante.php
+++ materiaux/cendre_volante.php
@@ -84,18 +84,63 @@
 	</div>
 </nav>
 <div class="content">
-			<li><a href="deblais.php">Deblais</a></li>
-			<li><a href="">Materiaux</a></li>
-			<li><a href="">Materiaux</a></li>
-			<li><a href="">Materiaux</a></li>
-			<li><a href="">Materiaux</a></li>
-			<li><a href="">Materiaux</a></li>
-			<li><a href="">Materiaux</a></li>
-			<li><a href="">Materiaux</a></li>
 	
+	<form action="cendre_volante.php" method="get">
+		<h2>trier par :</h2>
+	    <input type="radio" name="tri" value="prix" checked>Par prix
+
+		<input type="radio" name="tri" value="ID">Par ID
+		
+		<input class ="btn"type="submit"></br></br>
+	</form>
+
+	<h1>cendre_volante</h1>
+
+	<?php 
+
+	include('function.php');
+	
+	if (isset($_GET['tri'])) // On a le nom et le prénom
+	{
+		$tri=$_GET['tri'];
+		$sql="SELECT * FROM materiaux WHERE type = 'cendre_volante'  ORDER BY " . $tri;
+	}
+	else // Il manque des paramètres, on avertit le visiteur
+	{
+		$tri = 'prix';
+		$sql="SELECT * FROM materiaux WHERE type = 'cendre_volante'  ORDER BY " . $tri;
+	}
+
+		
+		
+	
+	
+
+	  
+		$req = $db->prepare($sql);
+		$req->execute();
+		  
+		$result = $req->fetchAll(PDO::FETCH_ASSOC);
+
+		foreach($result as $val){
+			echo $val['type']."<br>";
+			echo "prix : ".$val['prix']." euros <br>";
+			echo "adresse : ".$val['adresse']." ".$val['ville']."<br>";
+			?>
+			<a href="annonce.php?annonce=<?php echo $val['ID']; ?>">Voir l'annonce</a><br><br>
+
+			<?php
+			
+
+			
+		
+	}?>
+	
+
+	</div>	
 
 </div>
 
-		
+	<script type="text/javascript" src="../js/script.js"></script>
 	</body>
 </html>
