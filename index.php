8bf8fac15337e5ef9f9028c416eea2903db94d63
timothee <tim.dorand@gmail.com>
2015-12-22 15:33:30 +0100

    15h33 commit

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php require('header.php'); ?>
+<?php
+require('header.php'); ?>
 
 
 
--- inscription_field.php
+++ inscription_field.php
@@ -27,63 +27,7 @@
 //commentaire
 
 
-
 </script>
-
-<div class=''>
-
-
-
-	<div id="client">
-		<form method="POST" action="insert_user.php">
-			<h2>Inscription Client</h2>
-					<div class="form-input">
-							
-						<p>Entreprise/pseudo : <input type="text" name="pseudo" pattern="[a-zA-Z0-9]+" placeholder="Votre nom" required ></p>
-								
-							
-					</div>
-					<div class="form-input">
-							
-						<p>Mot de passe : <input type="password" name="password" pattern"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" placeholder="Votre mot de passe" required ></p>
-								
-							
-					</div>
-					<div class="form-input">
-							
-						<p>Email : <input type="email" name="mail" placeholder="Votre email"  required ></p>
-								
-							
-					</div>
-					<div class="form-input">
-							
-						<p>Adresse : <input type="text" name="adresse" pattern="[a-zA-Z0-9 ]+" placeholder="Votre adresse" required /></p>
-								
-							
-					</div>
-					<div class="form-input">
-							
-						<p>Code postal : <input type="text" name="postal" pattern="[0-9]{5}" placeholder="Votre code postal" required ></p>
-								
-							
-					</div>
-					<div class="form-input">
-							
-						<p>Ville : <input type="text" name="ville" pattern="[a-zA-Z0-9]+" placeholder="Votre ville" required ></p>
-								
-							
-					</div>
-
-					<div class="role">
-
-						<input type="number" name="role" value="4">
-
-					</div>
-
-			<input class ="btn"type="submit"></br></br>
-			</form>
-
-	</div>
 
 
 	<div class="inscription">
@@ -93,11 +37,47 @@
 		<a href="" onclick="fournisseur();return false">Inscription fournisseur</a>
 	</div>
 
-
-
-	<div id="fournisseur">
+<div>
+	<div class="form_inscription" id="client">
 		<form method="POST" action="insert_user.php">
-			<h2>- Inscription entreprise -</h2>
+			<h2>Inscription Client</h2>
+					<div class="form-input">
+						<p>Entreprise/pseudo : <input type="text" name="pseudo" pattern="[a-zA-Z0-9]+" placeholder="Votre nom" required ></p>
+					</div>
+
+					<div class="form-input">
+						<p>Mot de passe : <input type="password" name="password" pattern"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" placeholder="Votre mot de passe" required ></p>
+					</div>
+
+					<div class="form-input">
+						<p>Email : <input type="email" name="mail" placeholder="Votre email"  required ></p>
+					</div>
+
+					<div class="form-input">
+						<p>Adresse : <input type="text" name="adresse" pattern="[a-zA-Z0-9 ]+" placeholder="Votre adresse" required /></p>
+					</div>
+
+					<div class="form-input">
+						<p>Code postal : <input type="text" name="postal" pattern="[0-9]{5}" placeholder="Votre code postal" required ></p>
+					</div>
+
+					<div class="form-input">
+						<p>Ville : <input type="text" name="ville" pattern="[a-zA-Z0-9]+" placeholder="Votre ville" required ></p>
+					</div>
+
+					<div class="role">
+						<input type="number" name="role" value="4">
+					</div>
+
+			<input class ="btn"type="submit"></br></br>
+			</form>
+
+	</div>
+
+
+	<div class="form_inscription" id="fournisseur">
+		<form method="POST" action="insert_user.php">
+			<h2>Inscription entreprise</h2>
 					<div class="form-input">
 							
 						<p>entreprise : <input type="text" name="pseudo" pattern="[a-zA-Z0-9]+" placeholder="Votre nom" required ></p>
@@ -139,17 +119,14 @@
 
 						<input type="number" value="3">
 
-					</div> 
-						
-						
-						
+					</div>
 
-		
-		
-		
-		
+
 			<input class ="btn"type="submit"></br></br>
 		</form>
 	</div>
+
+
+
 </div>
 
