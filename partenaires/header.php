aba95591c2cd42a8218088da613f0eeb143b66a6
timothee <tim.dorand@gmail.com>
2016-01-30 19:38:37 +0100

    Léger changements pour faire fonctionner sous mac. (problèmes des warning)

--- partenaires/header.php
+++ partenaires/header.php
@@ -1,4 +1,6 @@
+<?php 		require ('../config.php');
 
+?>
 <html>
 <head>
 
@@ -57,14 +59,14 @@
 			echo '<li><a value="Accueil" href="../index.php"><span>Accueil</span></a></li>';
 			echo '<li><a value="Contact" href="../materiaux/">Matériaux</a></li>';
 			echo '<li><a value="Bars" href="index.php">Nos partenaires</a></li>';
-			echo '<li><a  value="Se connecter" href="../connection.php"><span>Se connecter</span></a></li>';
+			echo '<li><a  value="Se connecter" href="../connection.php"><span>Connexion</span></a></li>';
 			echo '<li><a  value="inscription" href="../inscription.php"><span>Inscription</span> </a></li>';
 
 		}else{
 			echo '<li><a value="Accueil" href="../index.php">Accueil</a></li>';
 			echo '<li><a value="Contact" href="../materiaux/">Matériaux</a></li>';
 			echo '<li><a value="Bars" href="index.php">Nos partenaires</a></li>';
-			echo '<li><a value="deconnection" href="../deconnection.php">se déconnecter</a></li>';
+			echo '<li><a value="deconnection" href="../deconnection.php">Déconnexion</a></li>';
 			echo '<li><a href="../single_user.php">'.$_SESSION["name"].'</a></li>';
 		}
 		?>
--- single_user.php
+++ single_user.php
@@ -25,14 +25,14 @@
 						echo '<li><a value="Accueil" href="index.php"><span>Accueil</span></a></li>';
 						echo '<li><a value="Contact" href="materiaux.php">Matériaux</a></li>';
 						echo '<li><a value="Bars" href="partenaires.php">Nos partenaires</a></li>';
-						echo '<li><a  value="Se connecter" href="connection.php"><span>Se connecter</span></a></li>';
+						echo '<li><a  value="Se connecter" href="connection.php"><span>Connexion</span></a></li>';
 						echo '<li><a  value="inscription" href="inscription.php"><span>Inscription</span> </a></li>';
 
 					}else{
 						echo '<li><a value="Accueil" href="index.php">Accueil</a></li>';
 						echo '<li><a value="Contact" href="materiaux.php">Matériaux</a></li>';
 						echo '<li><a value="Bars" href="partenaires.php">Nos partenaires</a></li>';
-						echo '<li><a value="deconnection" href="deconnection.php">se déconnecter</a></li>';
+						echo '<li><a value="deconnection" href="deconnection.php">Déconnexion</a></li>';
 						echo '<li><a>'.$_SESSION["name"].'</a></li>';
 					}
 				?>
